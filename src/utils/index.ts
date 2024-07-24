35629f9325ef93ac84955e11e9b5a04690f6ad8c
Yashaswi Agrawal <yashaswi@imagekit.io>
2024-07-24 16:22:32 +0530

    regression fixed

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -207,7 +207,7 @@ export function removeTrailingSlash(path: string | undefined): string {
     return '';
   }
 
-  path.replace(/\/$/, '');
+  path = path.replace(/\/$/, '');
   path = path.replace(/\\$/, '');
   return path;
 }
